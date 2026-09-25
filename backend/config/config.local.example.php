2aac8d647eb4239466c7a0086c691524c6a6e6b5
Hriday-Aneja <hridayaneja13@gmail.com>
2026-09-25 16:33:25 +0530

    landing page

--- backend/config/config.local.example.php
+++ backend/config/config.local.example.php
@@ -4,5 +4,5 @@ define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
 define('DB_NAME', 'queueless');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 define('DB_CHARSET', 'utf8mb4');
